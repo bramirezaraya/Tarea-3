3b8dabd9cade695c108c476a49fd82d9b12c3fa7
Bryan Ramirez <bryan.ramirez.a@mail.pucv.cl>
2021-10-20 17:19:49 -0300

    Version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {InicioComponent} from './inicio/inicio.component';
+import {ContactComponent} from './contact/contact.component';
+
+const routes: Routes = [
+  
+  {path:"", component:InicioComponent},
+  {path:"contact", component:ContactComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
